d843a5a36f2053474a030ecdbed9ee399821a298
Vithusan Kanageswaran <vk@Vithusans-MacBook-Air.local>
2019-11-06 13:38:58 -0500

    added all calls in the type controllers

---
+++ controllers/type.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const typeApi = require('../models/type.js')
+
+const typeRouter = express.Router()
+
+typeRouter.get('/', async (req, res) => {
+    const allType = await typeApi.getAllType()
+    res.json(allType)
+})
+
+typeRouter.get('/:typeId', async (req, res) => {
+    const singleType = await typeApi.getSingleType(req.params.typeId)
+    res.json(singleType)
+})
+
+typeRouter.post('/', async (req, res) => {
+    const newType = await typeApi.createType(req.body)
+    res.json(newType)
+})
+
+typeRouter.put('/:typeId', async (req, res) => {
+    const updatedType = await typeApi.updateType(req.params.typeId, req.body)
+    res.json(updatedType)
+})
+
+typeRouter.delete('/:typeId', async (req, res) => {
+    const deletedType = await typeApi.deleteType(req.params.typeId)
+    res.json(deletedType)
+})
+
+module.exports = {
+    typeRouter
+}
