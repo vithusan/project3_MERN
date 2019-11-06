1fca8c735e350f57e28c106e59a4625155dc6904
Vithusan Kanageswaran <vk@Vithusans-MacBook-Air.local>
2019-11-06 10:55:00 -0500

    installed node-module and add .gitignore file

---
+++ client/build/precache-manifest.21aa161e5df948e43899191d0d26a368.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b3bf1e6ac9f8b436a5aec1e7087d104",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edc1338d87904d857a22",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f8d2d9344fe2689b427f",
+    "url": "/static/js/2.2f9dfdf9.chunk.js"
+  },
+  {
+    "revision": "edc1338d87904d857a22",
+    "url": "/static/js/main.4388e5e6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
