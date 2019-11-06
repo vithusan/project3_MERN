a513734becd03814ad6dff9ff6a09cd092170fb6
Vithusan Kanageswaran <vk@Vithusans-MacBook-Air.local>
2019-11-06 10:51:49 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+
+const { templateRouter } = require('./controllers/template.js')
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use(express.json())
+
+
+app.use(express.static(`${__dirname}/client/build`))
+
+app.use('/api/helloworld', templateRouter)
+
+app.get('/*', (req, res) => {
+    res.sendFile(`${__dirname}/client/build/index.html`)
+})
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+    console.log(`App is listening on PORT ${PORT}`)
+})
