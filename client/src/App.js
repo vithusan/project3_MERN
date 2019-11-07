ccb611be49cc8c61c6b42056d94ccce19374b315
Vithusan Kanageswaran <vk@Vithusans-MacBook-Air.local>
2019-11-06 20:50:23 -0500

    update the route to point to type component as the landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import HelloWorld from './components/HelloWorld.js'
+import Type from './components/Type.jsx'
 import './App.css';
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path="/" component={HelloWorld}/>
+          <Route exact path="/" component={Type} />
         </Switch>
       </Router>
     </div>
