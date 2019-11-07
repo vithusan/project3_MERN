08c3e06fd6112e82a232fbaf939cd1fe08e6488e
Vithusan Kanageswaran <vk@Vithusans-MacBook-Air.local>
2019-11-06 20:16:11 -0500

    added all the calls in the brand controllers

---
+++ controllers/brand.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const brandApi = require('../models/brand.js')
+
+const brandRouter = express.Router()
+
+brandRouter.get('/', async (req, res) => {
+    const allBrand = await brandApi.getAllBrand()
+    res.json(allBrand)
+})
+
+brandRouter.get('/:brandId', async (req, res) => {
+    const singleBrand = await brandApi.getSingleBrand(req.params.brandId)
+    res.json(singleBrand)
+})
+
+brandRouter.post('/', async (req, res) => {
+    const newBrand = await brandApi.createBrand(req.body)
+    res.json(newBrand)
+})
+
+brandRouter.put('/:brandId', async (req, res) => {
+    const updatedBrand = await brandApi.updateBrand(req.params.brandId, req.body)
+    res.json(updatedBrand)
+})
+
+brandRouter.get('/:brandId', async (req, res) => {
+    const deletedBrand = await brandApi.deleteBrand(req.params.brandId)
+    res.json(deletedBrand)
+})
+
+module.exports = {
+    brandRouter
+}
