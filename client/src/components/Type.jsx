deea01f40f6d57b1634cb4f8f5f25219c99ac453
Vithusan Kanageswaran <vk@Vithusans-MacBook-Air.local>
2019-11-08 10:28:49 -0500

    create callback function and delete callback function

--- client/src/components/Type.jsx
+++ client/src/components/Type.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import axios from 'axios'
+import { Link } from 'react-router-dom'
+import Brand from './Brand.jsx'
+import Item from './Item.jsx'
 
 class Type extends Component {
 
     state = {
+        type: {
+            name: '',
+            description: '',
+            imgUrl: ''
+        },
         typeList: []
-
     }
 
     componentDidMount = () => {
@@ -17,18 +24,47 @@ class Type extends Component {
         this.setState({ typeList: getAllType.data })
     }
 
+    createNewType = async () => {
+        const newType = {
+            name: this.state.type.name,
+            description: this.state.type.description
+        }
+
+        await axios.post('/api/type', newType)
+        this.refreshPage()
+    }
+    handleChange = (event) => {
+        event.preventDefault()
+        const prevState = { ...this.state.type }
+        prevState[event.target.name] = event.target.value
+        this.setState({ type: prevState })
+    }
+
+    deleteType = async (typeId) => {
+        await axios.delete(`/api/type/${typeId}`)
+        await this.refreshPage()
+    }
+
+
+
     render() {
 
         return (
             <div>
-                {this.state.typeList.map((type) => {
+                {this.state.typeList.map((type, index) => {
                     return (
                         <div>
-                            <h1>{type.name}</h1>
-                            <h2>{type.description}</h2>
+                            <Link to={`/type/${type._id}`}>{type.name}</Link>
+                            <button onClick={() => this.deleteType(type._id)}>Delete</button>
                         </div>
                     )
                 })}
+                <form onSubmit={() => this.createNewType()}>
+                    <input type="text" name="name" placeholder="name" onChange={this.handleChange} value={this.state.type.name} />
+                    <input type="text" name="description" placeholder="description" onChange={this.handleChange} value={this.state.type.description} />
+                    <input type="text" name="imgUrl" placeholder="ImgUrl" onChange={this.handleChange} value={this.state.type.imgUrl} />
+                    <button type="submit" >Create</button>
+                </form>
             </div>
         );
     }
